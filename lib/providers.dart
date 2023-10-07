545ab7bc797c2069ed9aee52e890fce26f4676da
Maxeem <jmax.posh@gmail.com>
2023-10-07 17:40:41 -0300

    add countriesRepositoryProvider

--- lib/providers.dart
+++ lib/providers.dart
@@ -2,5 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:randstad_flutter_assessment/repositories/countries_repository.dart';
 
-final countriesCapitalsProvider = FutureProvider((ref) =>
-    CountriesRepository(client: http.Client()).listCountriesCapitals());
+final countriesRepositoryProvider =
+    Provider((ref) => CountriesRepository(client: http.Client()));
+
+final countriesCapitalsProvider = FutureProvider(
+    (ref) => ref.read(countriesRepositoryProvider).listCountriesCapitals());
